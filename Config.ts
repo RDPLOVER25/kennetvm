5b00b720f335105c891cbe93973d0efbd913d22b
RDPLOVER25 <132683402+RDPLOVER25@users.noreply.github.com>
2024-02-15 23:07:41 +0200

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
     ChatSound: "//computernewb.com/collab-vm/notify.ogg",
     ServerAddresses: [
-        "wss://coolvm.ddns.net/collab-vm/vm3",
+        "ws://147.185.221.18:33646",
         "wss://psh3kf-6004.csb.app/",
         "wss://computernewb.com/collab-vm/vm2",
         "wss://computernewb.com/collab-vm/vm3",
