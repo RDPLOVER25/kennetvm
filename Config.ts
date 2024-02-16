9168b97932e0727cd7949a83919a3e9e6637fd81
RDPLOVER25 <132683402+RDPLOVER25@users.noreply.github.com>
2024-02-16 10:40:22 +0200

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
     ChatSound: "//computernewb.com/collab-vm/notify.ogg",
     ServerAddresses: [
-        "ws://arthurus36.loca.lt",
+        "wss://rat-driven-grizzly.ngrok-free.app",
         "wss://psh3kf-6004.csb.app/",
         "wss://computernewb.com/collab-vm/vm2",
         "wss://computernewb.com/collab-vm/vm3",
