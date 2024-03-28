447a42d73a411d24df255a6ce822e057b308276a
RDPLOVER25 <132683402+RDPLOVER25@users.noreply.github.com>
2024-03-28 17:34:01 +0200

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,7 +1,7 @@
 export const Config = {
     ChatSound: "//computernewb.com/collab-vm/notify.ogg",
     ServerAddresses: [
-        "wss://kennetvm1.zapto.org",
+        "wss://kennetvm1.us.to",
         "wss://fa9flixm63ds.share.zrok.io",
         "wss://qrot96rgq6k5.share.zrok.io",
         "wss://7kdywvzr8itu.share.zrok.io",
